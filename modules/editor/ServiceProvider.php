d6f6e8ac8b1f6414ab865314e5af9ecf5c89defe
Thijmen2006 <169696@student.horizoncollege.nl>
2023-06-05 11:56:56 +0200

    composer install

--- modules/editor/ServiceProvider.php
+++ modules/editor/ServiceProvider.php
@@ -1,13 +1,16 @@
 <?php namespace Editor;
 
+use Backend;
+use Backend\Models\UserRole;
 use October\Rain\Support\ModuleServiceProvider;
 
+/**
+ * ServiceProvider for Editor module
+ */
 class ServiceProvider extends ModuleServiceProvider
 {
     /**
-     * Register the service provider.
-     *
-     * @return void
+     * register the service provider.
      */
     public function register()
     {
@@ -15,12 +18,45 @@ class ServiceProvider extends ModuleServiceProvider
     }
 
     /**
-     * Bootstrap the module events.
-     *
-     * @return void
+     * boot the module events.
      */
     public function boot()
     {
         parent::boot('editor');
+    }
+
+    /**
+     * registerNavigation
+     */
+    public function registerNavigation()
+    {
+        return [
+            'editor' => [
+                'label' => 'editor::lang.editor.menu_label',
+                'icon' => 'icon-pencil',
+                'iconSvg' => 'modules/editor/assets/images/editor-icon.svg',
+                'url' => Backend::url('editor'),
+                'order' => 90,
+                'permissions' => [
+                    'editor'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * registerPermissions
+     */
+    public function registerPermissions()
+    {
+        return [
+            'editor' => [
+                'label' => 'Access the Editor Tool',
+                'comment' => 'editor::lang.permissions.access_editor',
+                'tab' => 'Editor',
+                'roles' => UserRole::CODE_DEVELOPER,
+                'order' => 100
+            ],
+        ];
     }
 }
